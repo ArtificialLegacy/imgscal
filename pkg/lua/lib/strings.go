68f8dc23e1421c4ed51fbebd18f59b74ce17139e
Joseph <jgoyer3@gmail.com>
2026-03-25 22:09:18 -0400

    expand strings library

--- pkg/lua/lib/strings.go
+++ pkg/lua/lib/strings.go
@@ -1,6 +1,8 @@
 package lib
 
 import (
+	"strings"
+
 	"github.com/ArtificialLegacy/imgscal/pkg/log"
 	"github.com/ArtificialLegacy/imgscal/pkg/lua"
 	golua "github.com/yuin/gopher-lua"
@@ -87,5 +89,142 @@ func RegisterStrings(r *lua.Runner, lg *log.Logger) {
 
 			state.Push(golua.LString(runeList))
 			return 1
+		})
+
+	/// @func compare(a, b) -> string
+	/// @arg a {string}
+	/// @arg b {string}
+	/// @returns {int} - 0 if a == b, -1 if a < b, and 1 if a > b.
+	lib.CreateFunction(tab, "compare",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "a"},
+			{Type: lua.STRING, Name: "b"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			a := args["a"].(string)
+			b := args["b"].(string)
+
+			value := strings.Compare(a, b)
+
+			state.Push(golua.LNumber(value))
+			return 1
+		})
+
+	/// @func has_prefix(s, prefix) -> bool
+	/// @arg s {string}
+	/// @arg prefix {string}
+	/// @returns {bool}
+	lib.CreateFunction(tab, "has_prefix",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "s"},
+			{Type: lua.STRING, Name: "prefix"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			s := args["s"].(string)
+			prefix := args["prefix"].(string)
+
+			found := strings.HasPrefix(s, prefix)
+
+			state.Push(golua.LBool(found))
+			return 1
+		})
+
+	/// @func trim_prefix(s, prefix) -> string
+	/// @arg s {string}
+	/// @arg prefix {string}
+	/// @returns {string}
+	lib.CreateFunction(tab, "trim_prefix",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "s"},
+			{Type: lua.STRING, Name: "prefix"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			s := args["s"].(string)
+			prefix := args["prefix"].(string)
+
+			after := strings.TrimPrefix(s, prefix)
+
+			state.Push(golua.LString(after))
+			return 1
+		})
+
+	/// @func cut_prefix(s, prefix) -> string, bool
+	/// @arg s {string}
+	/// @arg prefix {string}
+	/// @returns {string}
+	/// @returns {bool}
+	lib.CreateFunction(tab, "cut_prefix",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "s"},
+			{Type: lua.STRING, Name: "prefix"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			s := args["s"].(string)
+			prefix := args["prefix"].(string)
+
+			after, found := strings.CutPrefix(s, prefix)
+
+			state.Push(golua.LString(after))
+			state.Push(golua.LBool(found))
+			return 2
+		})
+
+	/// @func has_suffix(s, suffix) -> bool
+	/// @arg s {string}
+	/// @arg suffix {string}
+	/// @returns {bool}
+	lib.CreateFunction(tab, "has_suffix",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "s"},
+			{Type: lua.STRING, Name: "suffix"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			s := args["s"].(string)
+			suffix := args["suffix"].(string)
+
+			found := strings.HasSuffix(s, suffix)
+
+			state.Push(golua.LBool(found))
+			return 1
+		})
+
+	/// @func trim_suffix(s, suffix) -> string
+	/// @arg s {string}
+	/// @arg suffix {string}
+	/// @returns {string}
+	lib.CreateFunction(tab, "trim_suffix",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "s"},
+			{Type: lua.STRING, Name: "suffix"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			s := args["s"].(string)
+			suffix := args["suffix"].(string)
+
+			after := strings.TrimSuffix(s, suffix)
+
+			state.Push(golua.LString(after))
+			return 1
+		})
+
+	/// @func cut_suffix(s, suffix) -> string, bool
+	/// @arg s {string}
+	/// @arg suffix {string}
+	/// @returns {string}
+	/// @returns {bool}
+	lib.CreateFunction(tab, "cut_suffix",
+		[]lua.Arg{
+			{Type: lua.STRING, Name: "s"},
+			{Type: lua.STRING, Name: "suffix"},
+		},
+		func(state *golua.LState, d lua.TaskData, args map[string]any) int {
+			s := args["s"].(string)
+			suffix := args["suffix"].(string)
+
+			after, found := strings.CutSuffix(s, suffix)
+
+			state.Push(golua.LString(after))
+			state.Push(golua.LBool(found))
+			return 2
 		})
 }
